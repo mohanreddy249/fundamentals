399062ff07c5c53214261ad13936e7b004dcc6f0
Mohan reddy <srisaireddy2003@gmail.com>
2019-06-24 20:08:53 +0530

    mohan reddy249

--- Mohan Reddy_249/src/mohan/values.java
+++ Mohan Reddy_249/src/mohan/values.java
@@ -1,10 +1,9 @@
 package mohan;
 
-public class wipro {
+public class values {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(args[0]+" Technologies "+args[1]);
 
 	}
 
