a27532539a10380cdfe160be1b6cb838db91b5d3
Mohan reddy <srisaireddy2003@gmail.com>
2019-06-24 16:54:31 +0530

    f hjsdhfbhsdjBAJ

---
+++ Mohan reddy/src/mohan/wipro.java
@@ -0,0 +1,11 @@
+package mohan;
+
+public class wipro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0]+" Technologies "+args[1]);
+
+	}
+
+}
